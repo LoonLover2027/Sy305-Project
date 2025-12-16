e0f8a32f8d49c8f40bd5c347de86e300532f7372
Chesler <m271077@usna.edu>
2025-12-16 17:40:40 -0500

    I updated the client code that needed comments

--- Client/clientC2.c
+++ Client/clientC2.c
@@ -12,15 +12,15 @@
 
 int main(void){
  
-  char *ip = "10.60.117.170";
+  char *ip = "10.60.117.170";   // establish IP address to send to
   int port = PORT;
- 
-  int sockfd;
+  
+  int sockfd;                   // initialize the socket struct. 
   struct sockaddr_in addr;
   char buffer[1024] = {0};
   socklen_t addr_size;
 
-  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);    // initialize the UDP socket to send information to the server.
   memset(&addr, '\0', sizeof(addr));
   addr.sin_family = AF_INET;
   addr.sin_port = htons(port);
@@ -28,23 +28,22 @@ int main(void){
 
   bzero(buffer, 1024);
 
-
+  // ---------------
+  // Client Command and Control
+  // ---------------
 
   while(1)
   {	  
-	  printf("Message options: 'DEPLOY', 'EXECUTE', 'STATUS', 'KILL'\n");
+	  printf("Message options: 'DEPLOY', 'EXECUTE', 'STATUS', 'KILL'\n");                       
     printf(">>");
-    if(fgets(buffer, sizeof(buffer),stdin) != NULL){
+    if(fgets(buffer, sizeof(buffer),stdin) != NULL){                                        // Gets the message to send and saves it in the buffer to then be sent to the server. 
       buffer[strcspn(buffer, "\n")] = '\0';
-      sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&addr, sizeof(addr));
+      sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&addr, sizeof(addr));     // send the message to the server.
     } else{
       printf("Error sending.");
     }
-	  printf("[+]Data sent: %s\n", buffer);
+	  printf("[+]Data sent: %s\n", buffer);     // print out the message sent. 
     bzero(buffer, 1024);
-	  // addr_size = sizeof(addr);
-	  // recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*)&addr, &addr_size);
-	  // printf("[+]Data recv: %s\n", buffer);
   }
   return 0;
 }
